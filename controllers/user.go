c16101393dfbbe862f3f846fc9527be0dd20ccac
Yiğitalp <Yiitalp>
2023-05-02 19:28:12 +0300

    first commit

---
+++ controllers/user.go
@@ -0,0 +1,87 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+
+	"project/models"
+
+	"github.com/gin-contrib/sessions"
+)
+
+type AuthController struct {
+	db *gorm.DB
+}
+
+func NewAuthController(db *gorm.DB) *AuthController {
+	return &AuthController{db: db}
+}
+
+func (ac *AuthController) Register(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Hash password before storing it in the database
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	user.Password = string(hashedPassword)
+
+	if err := ac.db.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User registered successfully"})
+}
+
+func (ac *AuthController) Login(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Retrieve user record from database
+	var dbUser models.User
+	if err := ac.db.Where("username = ?", user.Username).First(&dbUser).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	// Compare hashed passwords
+	if err := bcrypt.CompareHashAndPassword([]byte(dbUser.Password), []byte(user.Password)); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	// Set session cookie
+	session := sessions.Default(c)
+	session.Set("user_id", dbUser.ID)
+	if err := session.Save(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User logged in successfully"})
+}
+
+func (ac *AuthController) Logout(c *gin.Context) {
+	// Clear session cookie
+	session := sessions.Default(c)
+	session.Clear()
+	if err := session.Save(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User logged out successfully"})
+}
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"project/controllers"
+	"project/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := setupRouter()
+	_ = r.Run(":8080")
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	db := database.InitDb()
+	authController := controllers.NewAuthController(db)
+
+	r.POST("/register", authController.Register)
+	r.POST("/login", authController.Login)
+	r.POST("/logout", authController.Logout)
+
+	return r
+
+}
